c79bfd8643b002832a8d28f70713a78bdff8af31
le.duy.ngoc <le.duy.ngoc2@systemi.co.jp>
2023-07-05 17:33:38 +0700

    v1 ui

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// $("button").click(function() {
+//     setTimeout(explode, 5000);
+//     reset();
+//   })
+
+//   function reset() {
+//     $("img").attr("src", "https://bit.ly/2cKqT6s");
+//   }
+  
+//   function explode() {
+//     $("img").attr("src", "https://bit.ly/2d5W77w");
+//   }
+  
+$(".banner > img:gt(0)").hide();
